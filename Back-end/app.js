0d9a246143aa859fcde14e85c78df3f7e1f25c70
BenC92 <63462413+BenC92@users.noreply.github.com>
2021-03-31 15:48:42 +0100

    third part half way

--- Back-end/app.js
+++ Back-end/app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -29,5 +30,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
