356e37b102dd15e6bbe4271ff862e9a701ac602b
BenC92 <63462413+BenC92@users.noreply.github.com>
2021-04-06 17:11:08 +0100

    half and abit

--- Back-end/app.js
+++ Back-end/app.js
@@ -4,7 +4,8 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const path = require('path');
 
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
@@ -28,6 +29,8 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
