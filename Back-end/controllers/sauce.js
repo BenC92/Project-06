dd730facb63c45f043e9f73e6a05753dbbc6ae93
BenC92 <63462413+BenC92@users.noreply.github.com>
2021-05-19 17:16:03 +0100

    just need to do final touch

--- Back-end/controllers/sauce.js
+++ Back-end/controllers/sauce.js
@@ -1,35 +1,35 @@
 const Sauce = require('../models/sauce');
-const fs =require('fs');
+const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
   const sourceData = JSON.parse(req.body.sauce);
   console.log(sourceData)
   const url = req.protocol + '://' + req.get('host');
-    const sauce = new Sauce({
-     name: sourceData.name,
-     manufacturer: sourceData.manufacturer,
-      description: sourceData.description,
-      mainPepper: sourceData.mainPepper,
-      imageUrl: url + '/images/' + req.file.filename,
-      userId: sourceData.userId,
-      heat: sourceData.heat,
-      usersLiked: [],
-      usersDislike: []
-    });
-    sauce.save().then(
-      () => {
-        res.status(201).json({
-          message: 'Post saved successfully!'
+  const sauce = new Sauce({
+    name: sourceData.name,
+    manufacturer: sourceData.manufacturer,
+    description: sourceData.description,
+    mainPepper: sourceData.mainPepper,
+    imageUrl: url + '/images/' + req.file.filename,
+    userId: sourceData.userId,
+    heat: sourceData.heat,
+    usersLiked: [],
+    usersDislike: []
+  });
+  sauce.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
       });
-        }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
- };
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
 
 
 exports.getOneSauce = (req, res, next) => {
@@ -48,18 +48,20 @@ exports.getOneSauce = (req, res, next) => {
   );
 };
 
-exports.modifySauce = (req, res, next) =>{
+exports.modifySauce = (req, res, next) => {
   console.log(req.body)
   console.log(req.file)
-	const sauceData = JSON.parse(req.body.sauce)
+  const sauceData = JSON.parse(req.body.sauce)
 
-	
-	if (req.file) {
-		sauceData.imageUrl = req.protocol + '://' + req.get('host') + '/images/' + req.file.filename;
-	} 
-	
-	
-	Sauce.updateOne({_id: req.params.id}, sauceData).then(
+
+  if (req.file) {
+    sauceData.imageUrl = req.protocol + '://' + req.get('host') + '/images/' + req.file.filename;
+  }
+
+
+  Sauce.updateOne({
+    _id: req.params.id
+  }, sauceData).then(
     () => {
       res.status(201).json({
         message: 'Sauce updated successfully!'
@@ -71,16 +73,20 @@ exports.modifySauce = (req, res, next) =>{
         error: error
       });
     }
-   );
+  );
 };
 
 
 exports.deleteSauce = (req, res, next) => {
-  Sauce.findOne({_id: req.params.id}).then(
+  Sauce.findOne({
+    _id: req.params.id
+  }).then(
     (sauce) => {
       const filename = sauce.imageUrl.split('/images/')[1];
       fs.unlink('images/' + filename, () => {
-        sauce.deleteOne({_id: req.params.id}).then(
+        sauce.deleteOne({
+          _id: req.params.id
+        }).then(
           () => {
             res.status(200).json({
               message: 'Deleted!'
@@ -114,37 +120,65 @@ exports.getAllSauce = (req, res, next) => {
 
 
 
-exports.likeSauce = (req, res, next) =>{
+exports.likeSauce = (req, res, next) => {
   console.log(req.body)
 
   const userId = req.body.userId
   const like = req.body.like
   const id = req.params.id
-  Sauce.findOne({ _id: id})
-    .then(sauce =>{
+  Sauce.findOne({
+      _id: id
+    })
+    .then(sauce => {
       const sauceData = {
         _id: id
       }
       const usersLiked = sauce.usersLiked.includes(userId)
       const usersDisliked = sauce.usersDisliked.includes(userId)
 
-      if (like >0 && !usersLiked){
+      if (like > 0 && !usersLiked) {
         sauceData.$inc = {
           likes: 1
         }
-        sauceData.$push = { usersLiked : userId}
-      } else if (less<0 && !usersDisliked){
+        sauceData.$push = {
+          usersLiked: userId
+        }
+      } else if (like < 0 && !usersDisliked) {
         sauceData.$inc = {
           dislike: -1
         }
-        sauceData.$pull= {usersDisliked : userId}
-      } 
-
-      sauce.updateOne({_id: id}, sauceData)
-    }).then(
-          (sauce) => {
-            res.status(200).json(sauce);
+        sauceData.$push = {
+          usersDisliked: userId
+        }
+      } else {
+        if (usersDisliked) {
+          sauceData.$inc = {
+            dislikes: -1
           }
+          sauceData.$pull = {
+            usersDisliked: userId
+          }
+        } else {
+          sauceData.$inc = {
+            likes: 1
+          }
+          sauceData.$pull = {
+            usersLiked: userId
+          }
+        }
+      }
+
+      console.log(sauceData)
+
+      sauce.updateOne({
+          _id: id
+        }, sauceData)
+        .then(
+          () =>
+          res.status(201).json({
+            message: 'Sauce has been liked! :)'
+          })
+
         ).catch(
           (error) => {
             res.status(404).json({
@@ -152,45 +186,5 @@ exports.likeSauce = (req, res, next) =>{
             });
           }
         );
-    };
-
-    exports.dislikeSauce = (req, res, next) =>{
-      console.log(req.body)
-    
-      const userId = req.body.userId
-      const dislike = req.body.dislike
-      const id = req.params.id
-      Sauce.findOne({ _id: id})
-        .then(sauce =>{
-          const sauceData = {
-            _id: id
-          } 
-          const usersDisliked = sauce.usersDisliked.includes(userId)
-          const usersLiked = sauce.usersLiked.includes(userId)
-         
-    
-          if (dislike >0 && !usersDisliked){
-            sauceData.$inc = {
-              dislikes: 1
-            }
-            sauceData.$push = { usersDisliked : userId}
-          } else if (less<0 && !usersLiked){
-            sauceData.$inc = {
-              like: -1
-            }
-            sauceData.$pull= {usersLiked : userId}
-          } 
-    
-          sauce.updateOne({_id: id}, sauceData)
-        }).then(
-              (sauce) => {
-                res.status(200).json(sauce);
-              }
-            ).catch(
-              (error) => {
-                res.status(404).json({
-                  error: error
-                });
-              }
-            );
-        };
+    })
+};
